0b9e886f81f8de0a68d9076bc77374dff5c54d5d
Fay Carsons <faycarsons@Mac.fios-router.home>
2025-07-08 18:00:25 -0400

    bleh

--- flake.nix
+++ flake.nix
@@ -1,78 +1,77 @@
 {
-  description = "NixOS for Raspberry Pi 4 with raspberry-pi-nix";
+  description = "Minimal NixOS for Raspberry Pi 4 using standard approach";
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    raspberry-pi-nix.url = "github:nix-community/raspberry-pi-nix";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
   
-  # Add binary cache configuration
+  # Add binary cache configuration for faster builds
   nixConfig = {
     extra-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCim8cyXW6WPI711V+Ji8vr9wTT5dBRdg=" ];
   };
   
-  outputs = { self, nixpkgs, raspberry-pi-nix }: 
-  let
-    inherit (nixpkgs.lib) nixosSystem;
-    
-    basic-config = { pkgs, lib, ... }: {
-      # Pi 4 board configuration - this is the key setting!
-      raspberry-pi-nix.board = "bcm2711";
-      
-      # Serial console parameters
-      boot.kernelParams = [
-        "console=ttyAMA0,115200"
-        "console=tty1"
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    nixosConfigurations.kiggymedia = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        # Import standard SD image module
+        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+        # Import Pi 4 hardware support
+        nixos-hardware.nixosModules.raspberry-pi-4
+        
+        ({ pkgs, lib, modulesPath, ... }: {
+          # Serial console parameters for debugging
+          boot.kernelParams = [
+            "console=ttyAMA0,115200"
+            "console=tty1"
+          ];
+          
+          # Disable GRUB (Pi uses different bootloader)
+          boot.loader.grub.enable = false;
+          boot.loader.generic-extlinux-compatible.enable = true;
+
+          # Basic networking
+          networking = {
+            hostName = "kiggymedia";
+            useDHCP = false;
+            interfaces.eth0.useDHCP = true;
+          };
+
+          # Enable SSH with root login for initial setup
+          services.openssh = {
+            enable = true;
+            settings.PermitRootLogin = "yes";
+          };
+
+          # Root user with SSH key
+          users.users.root = {
+            initialPassword = "root";
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpeoFztO2Jhgk0dIfV3s41H8qFCmy8YTBT1idaiD3Mm faycarsons23@gmail.com"
+            ];
+          };
+
+          # Hardware support
+          hardware.enableRedistributableFirmware = true;
+          
+          # Disable ZFS to avoid long build times
+          boot.supportedFilesystems = lib.mkForce [ "ext4" "vfat" ];
+
+          # Minimal packages
+          environment.systemPackages = with pkgs; [
+            vim
+            htop
+          ];
+
+          # Disable compression for faster builds  
+          sdImage.compressImage = false;
+
+          # System version
+          system.stateVersion = "24.11";
+        })
       ];
-      
-      # Basic networking
-      networking = {
-        hostName = "kiggymedia";
-        useDHCP = false;
-        interfaces = {
-          eth0.useDHCP = true;
-        };
-      };
-
-      # Enable SSH with root login
-      services.openssh = {
-        enable = true;
-        settings.PermitRootLogin = "yes";
-      };
-
-      # Root user with SSH key
-      users.users.root = {
-        initialPassword = "root";
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpeoFztO2Jhgk0dIfV3s41H8qFCmy8YTBT1idaiD3Mm faycarsons23@gmail.com"
-        ];
-      };
-
-      # Hardware support
-      hardware = {
-        enableRedistributableFirmware = true;
-        bluetooth.enable = false; # Keep it simple initially
-      };
-
-      # Minimal packages
-      environment.systemPackages = with pkgs; [
-        vim
-        htop
-      ];
-
-      # System version
-      system.stateVersion = "24.11";
-    };
-  in {
-    nixosConfigurations = {
-      kiggymedia = nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          raspberry-pi-nix.nixosModules.raspberry-pi
-          basic-config
-        ];
-      };
     };
   };
 }
